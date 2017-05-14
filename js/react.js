9a2c2829f219c036e942236fac6fecc05566ceef
arihito <arihito.m@gmail.com>
2017-05-14 16:53:54 +0900

    React.DOM div method wrapping

--- js/react.js
+++ js/react.js
@@ -1,6 +1,12 @@
 var Evaluator = React.createClass({
 	render: function() {
-		return React.DOM.h2(null, eval(this.props.text));
+		return React.DOM.div (
+		  null,
+		  React.DOM.input({
+		  	type: 'text'
+		  }),
+		  React.DOM.h2(null, 'result')
+		);
 	}
 });
 
