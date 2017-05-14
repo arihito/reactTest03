f4abed7a2be5b593f960e0afa637f78bb64fd56f
arihito <arihito.m@gmail.com>
2017-05-14 18:13:25 +0900

    lifecycle Method

--- js/react.js
+++ js/react.js
@@ -1,4 +1,33 @@
 var Evaluator = React.createClass({
+	componentWillMount: function () {
+		// ①描画時に通知
+		console.log('①描画時に通知 componentWillMount');
+	},
+	componentDidMount: function() {
+		// ②コンポーネントの描画時に1度だけ通知
+		console.log('②コンポーネントの描画時に1度だけ通知 componentDidMount');
+	},
+	componentWillReceiveProps: function(nextProps) {
+		// 新たなPropsが設定される度に実行
+		console.log('新たなPropsが設定される度に実行 componentWillReceiveProps:', nextProps);
+	},
+	shouldComponentUpdate: function(nextProps, nextState) {
+		// ③PropsやStateを受け取る直前に更新を管理
+		console.log('③PropsやStateを受け取る直前に更新を管理 shouldComponentUpdate:', nextProps, nextState);
+		return true;
+	},
+	componentWillUpdate: function(prevProps, prevState) {
+		// ④再描画直前に実行
+		console.log('④再描画直前に実行 componentWillUpdate:', prevProps, prevState);
+	},
+	componentDidUpdate: function(prevProps, prevState) {
+		// ⑤再描画直後に実行
+		console.log('⑤再描画直後に実行 componentDidUpdate:', prevProps, prevState);
+	},
+	componentWillUnmount: function() {
+		// ⑥閉じられる直前に後処理を実行
+		console.log('⑥閉じられる直前に後処理を実行 componentWillUnmount:');
+	},
 	getInitialState: function() {
 		return {
 			expression: ''
@@ -23,10 +52,6 @@ var Evaluator = React.createClass({
 });
 
 ReactDOM.render(
-  React.createElement(
-    Evaluator,
-    {
-    	text: '5 / 4'
-    }),
+  React.createElement(Evaluator),
   document.getElementById('content')
 );
